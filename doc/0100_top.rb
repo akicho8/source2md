206c9e257fa8d37820882b204d9369646f459b10
akicho8 <akicho8@gmail.com>
2023-07-17 13:16:07 +0900

    [docs][ci skip] Update README

--- doc/0100_top.rb
+++ doc/0100_top.rb
@@ -3,6 +3,12 @@ require "./setup"
 
 # Source2MD: Markdown generator from source code #
 
+## Basic Logic ##
+
+# 1. Divide the source code into paragraphs using blank lines as separations.
+# 1. Remove comments from the comment blocks.
+# 1. Include the code within the code block.
+
 ## Install ##
 
 # ```
